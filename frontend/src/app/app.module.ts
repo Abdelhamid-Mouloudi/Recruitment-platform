b0e66a1ba524c2822e02b930d096e5f7c512d185
Abdelhamid-Mouloud <abdelhamidmouloudi2003@gmail.com>
2024-09-29 01:58:47 +0100

    fixing bugs

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,10 +6,9 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SignupComponent } from './components/signup/signup.component';
 import { LoginComponent } from './components/login/login.component';
-import { OffersComponent } from './components/offers/offers.component';
-import { EmployerOffersComponent } from './components/employer-offers/employer-offers.component';
-import { AddJobOfferComponent } from './components/add-job-offer/add-job-offer.component';  // Importer ici
-import { AddInternshipOfferComponent } from './components/add-internship-offer/add-internship-offer.component';
+import { OffersModule } from './components/offers/offers.module';
+import { AddJobOfferModule } from './components/add-job-offer/add-job-offer.module';  // Importer ici
+import { AddInternshipOfferModule } from './components/add-internship-offer/add-internship-offer.module';
 import { EmployerApplicationsComponent } from './components/employer-applications/employer-applications.component';  // Importer ici
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatFormFieldModule } from '@angular/material/form-field';
@@ -18,25 +17,23 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSelectModule } from '@angular/material/select';
 import { PdfViewerModule } from 'ng2-pdf-viewer'; // Pour gérer les PDF
+
 @NgModule({
   declarations: [
     AppComponent,
     SignupComponent,
     LoginComponent,
-    OffersComponent,
-    EmployerOffersComponent,
-    AddJobOfferComponent,  // Assurez-vous que ce composant est bien déclaré ici
-    AddInternshipOfferComponent, EmployerApplicationsComponent  // Assurez-vous que ce composant est bien déclaré ici
+    EmployerApplicationsComponent  // Assurez-vous que ce composant est bien déclaré ici
   ],
   imports: [
     BrowserModule,
-     BrowserAnimationsModule, // Nécessaire pour Angular Material
-         MatFormFieldModule,
-         MatInputModule,
-         MatIconModule,
-         MatButtonModule,
-         MatSelectModule,
-         PdfViewerModule,
+    BrowserAnimationsModule, // Nécessaire pour Angular Material
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    MatButtonModule,
+    MatSelectModule,
+    PdfViewerModule,
     AppRoutingModule,
     ReactiveFormsModule,
     FormsModule,
